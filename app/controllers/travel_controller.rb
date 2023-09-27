46c19ad135fb696bc94d390c2e993715b4bde04c
Henry Ireland <henryireland95@gmail.com>
2023-09-27 11:23:42 +0100

    making changes

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -2,45 +2,43 @@ class TravelController < ApplicationController
   def index
   end
 
-  # def search
-  #   countries = find_country(params[:country])
-  #   unless countries
-  #     flash[:alert] = 'Country not found'
-  #     return render action: :index
-  #   end
-  #   @country = countries
-  #   # ...
-  # end
-
-  # def request_api(url)
-  #   response = JSON.parse(URI.open(url).read)
-  #   # return nil if response.status != 200
-  # end
-
-  # def find_country(name)
-  #   request_api(
-  #     "https://restcountries.com/v3.1/name/#{name}"
-  #   )
-  # end
-
   def search
-    country = find_country((params[:country]))
-    @country = country.read_body
+    countries = find_country(params[:country])
+    unless countries
+      flash[:alert] = 'Country not found'
+      return render action: :index
+    end
+
+    @country = countries.first
+    @weather = find_weather(@country['capital'], @country['alphacode'])
+    raise
   end
 
-  def find_country(name)
-    url = URI("https://rest-countries10.p.rapidapi.com/country/#{name}")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-
-    request = Net::HTTP::Get.new(url)
-    request["X-RapidAPI-Key"] = '447c1dbbb5msh9ad06a4c00be069p15e1a4jsnc720b8841080'
-    request["X-RapidAPI-Host"] = 'rest-countries10.p.rapidapi.com'
-
-    response = http.request(request)
+  def find_weather(city, country_code)
+    query = URI.encode("#{city},#{country_code}")
+    request_api(
+      "https://community-open-weather-map.p.rapidapi.com/forecast?q=#{query}"
+    )
   end
 
   private
+
+  def request_api(url)
+    response = Excon.get(
+      url,
+      headers: {
+        'X-RapidAPI-Host' => URI.parse(url).host,
+        'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
+      }
+    )
+    return nil if response.status != 200
+    JSON.parse(response.body)
+  end
+
+  def find_country(name)
+    request_api(
+      "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(name)}"
+    )
+  end
 
 end
